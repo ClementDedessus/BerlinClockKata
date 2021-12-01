8a6bc721e6030c6a578d3a04a9d38180a6891fb7
De dessus Clement <clement.dedessuslesmous@student.vinci.ipl.be>
2021-12-01 11:32:44 +0100

    refactor - .gitignore

--- berlinClockKata/vendor/composer/installed.php
+++ berlinClockKata/vendor/composer/installed.php
@@ -5,7 +5,7 @@
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => 'dbc7d8be37f283a895ad7e643950f9748264e939',
+        'reference' => '0541f6d819eee7fd10e97179c9f9867719682d83',
         'name' => '__root__',
         'dev' => true,
     ),
@@ -16,7 +16,7 @@
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => 'dbc7d8be37f283a895ad7e643950f9748264e939',
+            'reference' => '0541f6d819eee7fd10e97179c9f9867719682d83',
             'dev_requirement' => false,
         ),
         'doctrine/instantiator' => array(
@@ -41,12 +41,12 @@
             ),
         ),
         'nikic/php-parser' => array(
-            'pretty_version' => 'v4.13.1',
-            'version' => '4.13.1.0',
+            'pretty_version' => 'v4.13.2',
+            'version' => '4.13.2.0',
             'type' => 'library',
             'install_path' => __DIR__ . '/../nikic/php-parser',
             'aliases' => array(),
-            'reference' => '63a79e8daa781cac14e5195e63ed8ae231dd10fd',
+            'reference' => '210577fe3cf7badcc5814d99455df46564f3c077',
             'dev_requirement' => true,
         ),
         'phar-io/manifest' => array(
