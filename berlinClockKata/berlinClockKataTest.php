dbd41d9b4b0e367bcdeebfbbbb8432d1f9fdbb48
Clement De dessus <clement.dedessuslesmous@student.vinci.be>
2021-12-02 13:07:47 +0100

    refactor - testHours()

--- berlinClockKata/berlinClockKataTest.php
+++ berlinClockKata/berlinClockKataTest.php
@@ -22,7 +22,7 @@ class berlinClockKataTest extends TestCase
     public function testHours()
     {
         $hour = new berlinClockKata();
-        $actual = $hour->hours(3);
-        $this->assertEquals("111", $actual);
+        $actual = $hour->hours(0);
+        $this->assertEquals("", $actual);
     }
 }
