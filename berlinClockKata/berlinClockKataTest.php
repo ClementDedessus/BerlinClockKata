fac10cc0fa2cf4a781bef54da1f90b3ee770f698
leolamien <sienou.lamien@student.vinci.be>
2021-12-02 13:51:46 +0100

    refactor-testAllClock()

--- berlinClockKata/berlinClockKataTest.php
+++ berlinClockKata/berlinClockKataTest.php
@@ -39,7 +39,7 @@ class berlinClockKataTest extends TestCase
     public  function testAllClock()
     {
         $second = new berlinClockKata();
-        $actual = $second->heureEntiere(7,59,00);
-        $this->assertEquals("1 11 11111111111 1111 1", $actual);
+        $actual = $second->heureEntiere(15,00,00);
+        $this->assertEquals("111    1", $actual);
     }
 }
